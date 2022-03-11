d590c4639ac7b11d978d1a12a5e82090151fc3ce
Bikram Khanal <bikram.khanal@aiesec.net>
2022-03-11 16:15:12 -0600

    new soln

--- stringmatching.cpp
+++ stringmatching.cpp
@@ -1,24 +1,3 @@
-// while(True):
-//     try:
-//         to_find = input()
-//         if not to_find:break
-//         sentence = input()
-//         # print(sentence)
-//         if len (to_find) > len(sentence): pass
-//         else:    
-//             for i,char in enumerate(sentence):
-//                 if char == to_find[0]:
-//                     len_word = 0
-//                     len_word = i + len(to_find)
-//                     # print("Temp len: {} ".format(len_word))
-//                     if len_word <= len(sentence): 
-//                         # print("character: {} tofind: {} i: {} len of word: {}".format(char,to_find[0],i,len_word))
-//                         word = sentence[i:len_word]
-//                         # print("Word: ", word )
-//                         if word == to_find: print(i,end = " ")
-//             print("\n")
-//     except:break
-
   #include <iostream>
     #include <string>
     using namespace std;
@@ -26,14 +5,35 @@
     int main(){
     string to_find,sentence;
     bool go = true;
-    while (go){
-        cin >> to_find;
-        if (to_find.length() == 0) go = false;
-        // cout<<"herhe"<<endl;
-        cin >> sentence;
+    while (getline(cin,to_find)){
+      if (to_find.length() == 0){break;}
+        getline(cin,sentence);
         if (to_find.length()>sentence.length()) 
         ;
-
+        else{
+        for (int i = 0;i<sentence.length();i++){
+          // cout<<"sentence[i] "<<sentence[i]<<" to_find[0] "<< to_find[0]<<endl;
+          if (sentence[i] == to_find[0]){
+            int len_word = i + to_find.length();
+            // cout<<"Len_word "<<len_word<<endl;
+              string word = sentence.substr(i,to_find.length());
+              // cout<<"Word: "<<word<<" ";
+              if (word == to_find) cout<<i<<" ";
+            
+          }
+        }
+        cout<<endl;
+        }
     } 
 
     }
+
+//     p
+// Popup
+// helo
+// Hello there!
+// peek a boo
+// you speek a bootiful language
+// anas
+// bananananaspaj
+
